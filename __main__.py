e731ece8e1c15ced164238f31303d08c0a0a03e6
jmsdai <59010914+jmsdai@users.noreply.github.com>
2020-07-25 14:03:08 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
-from pre_processing import df_compile
+import pre_processing as pp
 
 def main():
     months = ["2020\\01\\", "2020\\02\\", "2020\\03\\", "2020\\04\\", "2020\\05\\", "2020\\06\\"]
     for month in months:
-        df_compile(month)
+        pp.df_compile(month)
 
 if __name__ == '__main__':
     main()
